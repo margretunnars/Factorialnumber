01ac0e8a30ec387ca0307322dd954b7131a78218
Margrét Unnarsdóttir <margretunnarsdottir@Margrets-MacBook-Pro.local>
2016-10-17 15:31:39 +0100

    first commit

---
+++ Factorialnumber/Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factorialnumber
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int val;
+			int factorialval = 1;
+			string input;
+
+			Console.WriteLine("Enter a number");
+			input = Console.ReadLine();
+			val = int.Parse(input);
+
+			for (int i = 1; i <= val; i++)
+			{
+				factorialval *= i;
+
+			}
+
+			Console.WriteLine("This is the factorial number is " + factorialval);
+		}
+	}
+}
